d55c8d693b172667388cb168060b70e8a130fc91
Alexander Barth <barth.alexander@gmail.com>
2023-08-21 09:42:27 +0200

    format table

--- src/litter_classification_post.jl
+++ src/litter_classification_post.jl
@@ -24,3 +24,44 @@ df[!,:file] .= @. last(splitdir(dirname(files)))
 
 
 display(select(df,:val_mean_IoU,:test_mean_IoU,:train_mean_IoU,:file))
+
+i = findmax(df.val_mean_IoU)[2]
+
+allnames = [
+    "activation",
+    "batchsize",
+    "nchannels",
+    "clip_grad_value",
+    "beta2",
+#    "std_noise_bands",
+    "lr",
+    "epoch",
+    "train_mean_IoU",
+    "val_mean_IoU",
+    "test_mean_IoU",
+]
+
+nicenames = Dict(
+    "activation" => "activation function",
+    "batchsize" => "batch size",
+    "nchannels" => "number of channels",
+    "clip_grad_value" => "gradient clipping threshold",
+#    "beta2",
+    "std_noise_bands" => "standard deviation of noise",
+    "lr" => "learning rate",
+    "epoch" => "number of epochs",
+#    "val_mean_IoU",
+#    "train_mean_IoU",
+#    "test_mean_IoU",
+)
+
+
+for n in allnames
+    nn = get(nicenames,n,n)
+    v = df[i,n]
+    if v isa AbstractFloat
+        v = round(v,digits=3)
+    end
+    println(nn,'\t',v)
+end
+df[i,:]
